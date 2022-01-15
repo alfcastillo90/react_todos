c88f9a1e86f7e5ec6885468cdf87bcbf26aa2a7a
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2022-01-15 14:32:21 -0300

    ADD: cambios pendientes

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { TodoContext } from '../TodoContext';
 import './TodoSearch.css';
-import {TodoContext} from "../TodoContext";
 
 function TodoSearch() {
   const { searchValue, setSearchValue } = React.useContext(TodoContext);
+
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
     setSearchValue(event.target.value);
   };
 
   return (
-    <input
-      className="TodoSearch"
-      placeholder="Cebolla"
-      value={searchValue}
-      onChange={onSearchValueChange}
-    />
+      <input
+          className="TodoSearch"
+          placeholder="Cebolla"
+          value={searchValue}
+          onChange={onSearchValueChange}
+      />
   );
 }
 
