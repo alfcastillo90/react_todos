eb72ecc646d27e60524dd91f5637f8c53bf2e42b
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2022-06-08 16:13:38 -0400

    ADD: action creators and implementation of use reducer

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -1,54 +1,102 @@
-import React from 'react';
+import React from "react";
 
 function useLocalStorage(itemName, initialValue) {
-  const [ synchronizedItem, setSynchronizedItem ] = React.useState(true)
-  const [ error, setError ] = React.useState(false);
-  const [ loading, setLoading ] = React.useState(true);
-  const [ item, setItem ] = React.useState(initialValue);
+    const [state, dispatch] = React.useReducer(reducer, initialState({ initialValue }))
 
-  React.useEffect(() => {
-    setTimeout(() => {
-      try {
-        const localStorageItem = localStorage.getItem(itemName);
-        let parsedItem;
+    const {
+        sincronizedItem,
+        loading,
+        error,
+        item,
+    } = state;
 
-        if (!localStorageItem) {
-          localStorage.setItem(itemName, JSON.stringify(initialValue));
-          parsedItem = initialValue;
-        } else {
-          parsedItem = JSON.parse(localStorageItem);
+    // Action creators
+    const onError = (error)=>dispatch({ type: actionTypes.error, payload: error});
+    const onSuccess = (parsedItem)=>dispatch({ type: actionTypes.success, payload: parsedItem});
+    const onSave = (newItem)=>dispatch({ type: actionTypes.save, payload: newItem});
+    const onSincronize = ()=>dispatch({ type: actionTypes.sincronize });
+
+    React.useEffect(()=>{
+        setTimeout( ()=>{
+        try {
+            const localStorageItem = localStorage.getItem(itemName);
+            let parsedItem;
+
+            if(!localStorageItem) {
+            localStorage.setItem(itemName,JSON.stringify(initialValue));
+            parsedItem = initialValue;
+            } else {
+            parsedItem = JSON.parse(localStorageItem);
+            }
+            onSuccess(parsedItem);
+        } catch (error) {
+            onError(error);
         }
-        setItem(parsedItem);
-        setLoading(false);
-        setSynchronizedItem(true);
-      } catch(error) {
-        setError(error);
-      }
-    }, 3000);
-  }, [ synchronizedItem ]);
+        },3000);
+    },[sincronizedItem])
 
-  const saveItem = (newItem) => {
-    try {
-      const stringifyItem = JSON.stringify(newItem);
-      localStorage.setItem(itemName, stringifyItem);
-      setItem(newItem);
-    } catch(error) {
-      setError(error);
+    const saveItem = (newItem) =>{
+        try {
+            const stringifiedItem = JSON.stringify(newItem);
+            localStorage.setItem(itemName,stringifiedItem);
+            onSave(newItem);
+        } catch (error) {
+            onError(error);
+        }
     }
-  };
 
-  const synchronizeItem = () => {
-    setLoading(true);
-    setSynchronizedItem(false);
-  }
+    const sincronizeItem = () =>{
+        onSincronize();
+    }
 
-  return {
-    item,
-    saveItem,
-    loading,
-    error,
-    synchronizeItem
-  };
+    return {
+        item, 
+        saveItem, 
+        loading,
+        error,
+        sincronizeItem,
+    };
 }
 
-export { useLocalStorage };
+const initialState = ({ initialValue })=>({
+    sincronizedItem: true,
+    loading: true,
+    error: false,
+    item :initialValue,
+});
+
+const actionTypes = {
+    error: 'ERROR',
+    success: 'SUCCESS',
+    save: 'SAVE',
+    sincronize: 'SINCRONIZE',
+}
+
+const reducerObject = (state, payload) =>({
+    [actionTypes.error]: {
+        ...state,
+        error: true,
+    },
+    [actionTypes.success]:{
+        ...state,
+        error:false,
+        sincronizedItem: true,
+        loading: false,
+        item: payload,
+    },
+    [actionTypes.save]:{
+        ...state,
+        item:payload
+    },
+    [actionTypes.sincronize]:{
+        ...state,
+        sincronizedItem: false,
+        loading: true,
+    },
+});
+
+const reducer = (state, action) =>{
+    return reducerObject(state, action.payload)[action.type] || state;
+}
+
+export { useLocalStorage }
